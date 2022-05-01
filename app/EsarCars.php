d4c420f285d581dd8af40a8e13d3a48a6d0630ca
Mina Alejandro <minaalejandro001@gmail.com>
2022-05-01 20:08:21 +1000

    car-data_list complete

--- app/EsarCars.php
+++ app/EsarCars.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class EsarCars extends Model
 {
     protected $table = 'esar_cars';
+    protected $fillable = [
+		'model_make_id', 'model_name', 'model_trim', 'model_year','model_transmission_type','model_body'
+	];
 }
--- app/Http/Controllers/CarDataController.php
+++ app/Http/Controllers/CarDataController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\EsarCars;
 
 class CarDataController extends Controller
 {
     public function getEsarCar(Request $request)
     {
-        $data = "123";
-       return json_encode($data);
+        $page = $request->get('page');
+        // $request->get('dates')
+        $results = $request->get('results');
+        if (empty($results)) {
+            $datas = EsarCars::select("*")->skip(($page-1)*10)->take(10)->get();
+        }
+        if ($results == 10 ){
+            $datas = EsarCars::select("*")->skip(($page-1)*10)->take(10)->get();
+        } else if ($results == 20) {
+            $datas = EsarCars::select("*")->skip(($page-1)*20)->take(20)->get();
+        } else if ($results == 50) {
+            $datas = EsarCars::select("*")->skip(($page-1)*50)->take(50)->get();
+        } else if ($results == 100) {
+            $datas = EsarCars::select("*")->skip(($page-1)*100)->take(100)->get();
+        };
+       
+        $meta = EsarCars::select("*")->count();
+        $total = ceil($meta / 10);
+        return response()->json(['data' => $datas, 'total'=>$total], 200);
     }
 }
